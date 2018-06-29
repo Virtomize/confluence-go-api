d4d3047032d846bb8f9ad83836d930f68eced1d6
Carsten Seeger <cseeger@epages.com>
2018-06-29 12:24:26 +0200

    fixed GPL Copyright date due to copy paste fail

--- structs.go
+++ structs.go
@@ -1,7 +1,7 @@
 /*
 	Go library for attlassians confluence wiki
 
-	Copyright (C) 2017 Carsten Seeger
+	Copyright (C) 2018 Carsten Seeger
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 	@author Carsten Seeger
-	@copyright Copyright (C) 2017 Carsten Seeger
+	@copyright Copyright (C) 2018 Carsten Seeger
 	@license http://www.gnu.org/licenses/gpl-3.0 GNU General Public License 3
 	@link https://github.com/cseeger-epages/confluence-go-api
 */
