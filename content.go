4800c7a9270233a6a3a63ae2313a3cfd59e5aaa3
Michael <88448147+headincl0ud@users.noreply.github.com>
2023-10-03 21:56:25 +0200

    Update content.go

--- content.go
+++ content.go
@@ -43,8 +43,8 @@ type Metadata struct {
 // Properties defines properties of the editor
 type Properties struct {
 	Editor                     *Editor                     `json:"editor"`
-	ContentAppearanceDraft     *ContentAppearanceDraft     `json:"content-appearance-draft"`
-	ContentAppearancePublished *ContentAppearancePublished `json:"content-appearance-published"`
+	ContentAppearanceDraft     *ContentAppearanceDraft     `json:"content_appearance_draft"`
+	ContentAppearancePublished *ContentAppearancePublished `json:"content_appearance_published"`
 }
 
 // Editor contains editor information
